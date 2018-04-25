1a5a917020815e974efa07a1eb5bb759ea84292c
mihan1235 <mihan1235@yandex.ru>
2018-04-25 23:09:11 +0300

    Solved problem with approximation

--- Integral/Int.cs
+++ Integral/Int.cs
@@ -15,28 +15,22 @@ namespace Integral
          * где F_i = F(a+i*h), h=(b-a)/n 
         */
 
-        public static double Integrate(tf f, double a, double b, double eps2)
+        public static double Integrate(tf f, double a, double b, double h)
         {
-            int n, i;
-            double sum, h, h1, x, s1, s2;
-            n = 1;
-            h = (b - a) / n;
-            sum = (f(a) + f(b)) / 2;
-            do
+            //h = Math.Pow(h,3);
+            h = h*h/100;
+            //Trapezoid method
+            //Kotesa formula
+            //int_a^b f(x) dx = h((f_0 + f_n)/2 + \sum_{i=1}^{n-1} f_i)
+            int n = (int)((b - a) / h + 1);
+            double sum =0;
+            sum += (f(a) + f(a+n*h))/2;
+            for (int i= 0; i < n-1; i++)
             {
-                n = n * 2;
-                h1 = h;
-                h = h / 2;
-                s1 = sum * h1;
-                x = a + h;
-                for (i = 1; i < n / 2; i++)
-                {
-                    sum = sum + f(x);
-                    x = x + h1;
-                }
-                s2 = sum * h;
-            } while (!(Math.Abs(s2 - s1) < eps2));
-            return s2;
+                sum += f(a+i*h);
+            }
+            sum *= h;
+            return sum;
         }
     }
 }
--- NumericalDerivative/Derivative.cs
+++ NumericalDerivative/Derivative.cs
@@ -12,12 +12,14 @@ namespace NumericalDerivative
 
         public static double FirstDerivative(tf f,double x, double h)
         {
-            return (f(x + h) - f(x)) / h;
+            //return (f(x + h) - f(x)) / h;
+            return 1 / (6 * h) * (-11 * f(x) + 18 * f(x + h) - 9 * f(x + 2 * h) + 2 * f(x + 3 * h));
         }
 
         public static double SecondDerivative(tf f, double x, double h)
         {
-            return (f(x) - 2*f(x+h) + f(x + 2*h)) / Math.Pow(h, 2);
+            //return (f(x) - 2*f(x+h) + f(x + 2*h)) / Math.Pow(h, 2);
+            return 1 / Math.Pow(h, 2) * (2*f(x) - 5 *f(x+h) + 4*f(x+2*h)-f(x+3*h));
         }
     }
 }
